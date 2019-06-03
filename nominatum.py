304e0ec73aabb0a7cf672fe61adc485f29b8690c
vvb-1997 <vinayvamanbhat@gmail.com>
2019-06-03 14:27:32 +0530

    Generalized the file
    Generalized the file

--- nominatum.py
+++ nominatum.py
@@ -1,43 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  3 10:42:17 2019
-
-@author: VBhat
-"""
-
 import pandas as pd
-import numpy as np
+import os
 
-path=r"D:\try\international\files\updated_files\GAM_INTL_data(04-09-19)_Jan-MarchData_validation_cleaned.xlsx"
+path=os.path.join("file","filename.xlsx")
 
 xls=pd.ExcelFile(path)
-data_china=pd.read_excel(xls,'POPAddress_checked')
 
-data_china=data_china[data_china['Exact_lat']=='None']
+data=pd.read_excel(xls,'Address_checked')
 
-data_china=data_china.dropna(subset=['Final_Cust_Long'])
+data=data[data['Exact_lat']=='None']
 
-#data_china=data_china[data_china['GAM_Region']=='AMEA']
+data=data.dropna(subset=['Final_Long'])
 
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(timeout=None,user_agent="my-application" )
 coun=[]
-for index,rows in data_china.iterrows():
-    if((rows["Final_Cust_Lat"]!="not found")|(rows["Final_Cust_Long"]!="not found")):
-        lat=rows['Final_Cust_Lat']
-        lon=rows['Final_Cust_Long']
+for index,rows in data.iterrows():
+    if((rows["Final_Lat"]!="not found")|(rows["Final_Long"]!="not found")):
+        lat=rows['Final_Lat']
+        lon=rows['Final_Long']
         location = geolocator.reverse((lat,lon),language='en')
         e=location.raw
         country=e['address']['country']
         coun.append(country)
         print(country)
-        print(index)
         print("-------------") 
-        data_china.loc[index,'new_Cust_Country']=e['address']['country'] 
+        data.loc[index,'Country']=e['address']['country'] 
 
+new_path=os.path.join("file","filename_with_address.xlsx")
 
-with pd.ExcelWriter(r"D:\try\international\files\updated_files\GAM_INTL_data(04-09-19)_Jan-MarchData_validation_cleaned_pop.xlsx") as writer:  
-    data_china.to_excel(writer, sheet_name='NNI')
+with pd.ExcelWriter(new_path) as writer:  
+    data.to_excel(writer, sheet_name='Updated')
     writer.save()
-
-
